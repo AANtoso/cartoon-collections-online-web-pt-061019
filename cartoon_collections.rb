ae77f4794a727ea64339d1cd90812ff8d8cad171
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-08 23:49:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)# code an argument here
   if array.any? {|word| word.length > 4}
     return true
   else
-    {|word| word.length < 4}
+    array.any? {|word| word.length < 4}
     return false
   end
   end
