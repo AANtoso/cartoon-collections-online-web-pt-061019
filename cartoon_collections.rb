beb06ba6e25c2949a121f2a59bf324e76644e0d9
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-09 00:01:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,5 +29,5 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |cheese|
     cheese.include?(cheese)
+    end
   end
-end
