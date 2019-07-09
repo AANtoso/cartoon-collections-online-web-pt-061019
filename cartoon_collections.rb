2ef73bff8072f8348e9371cacdc2a3ae6912a0b1
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-09 00:06:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,10 +27,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.find do |cheese|
+  cheese_types.find do |cheese|
     cheese.include?(cheese)
-  else
-    nil
-    end
   end
 end
